09ee10f1bea8b5a8c611e87f4a415bbeb852d3df
马凯 <kai.ma@connext.com.cn>
2020-08-25 10:07:54 +0800

    fixed 修复菜单部分跳转问题

--- main/src/layout/menu/sidebaritem.vue
+++ main/src/layout/menu/sidebaritem.vue
@@ -4,7 +4,7 @@
 <template lang='pug'>
     div.wrapper
         template(v-for='el in routes', v-if='!el.hidden&&el.children && el.children.length')
-            el-menu-item(v-if='hasOneShowingChildren(el.children) && !el.children[0].children', :index="el.children[0].path", :key='el.children[0].name', @click="goToUrl(el.module, el.path, el.children[0].path)")
+            el-menu-item(v-if='hasOneShowingChildren(el.children) && !el.children[0].children', :index="el.path+el.children[0].path", :key='el.children[0].name', @click="goToUrl(el.module, el.path, el.children[0].path)")
                 span(v-if="el.children[0].meta&&el.children[0].meta.icon" :class="el.children[0].meta.icon")
                 span(v-if="el.children[0].meta&&el.children[0].name" slot="title") {{el.children[0].name}}
             el-submenu(v-else :index="el.name||el.path" :key="el.name")
@@ -12,7 +12,7 @@
                     span(v-if="el.meta&&el.meta.icon" :class="el.meta.icon")
                     span(v-if="el.meta&&el.name" slot="title") {{el.name}}
                 template(v-for="child in el.children" v-if="!child.hidden")
-                    el-menu-item(:index="child.path", :key="child.name", @click="goToUrl(el.module, el.path, child.path)")
+                    el-menu-item(:index="el.path+child.path", :key="child.name", @click="goToUrl(el.module, el.path, child.path)")
                         span(v-if="child.meta&&child.meta.icon" :icon-class="child.meta.icon")
                         span(v-if="child.meta&&child.name" slot="title") {{child.name}}
 </template>
@@ -53,7 +53,7 @@
             goToUrl: _.debounce((title, herf, shref) => {
                 if (title.includes(Conf.mainApp)) {
                     ME.$router.push({
-                        path: shref
+                        path: herf+shref
                     })
                     return
                 }
